9ebda5a2cb25534e14887dc514d10508358e2050
sohag <mahbubulsohag810@gmail.com>
2022-11-17 08:26:21 +0600

    first commit

---
+++ src/Routes/Routes/Routes.js
@@ -0,0 +1,47 @@
+import { createBrowserRouter } from "react-router-dom";
+import DashboardLayout from "../../Layout/DashboardLayout";
+import Main from "../../Layout/Main";
+import Appointment from "../../Pages/Appointment/Appointment/Appointment";
+import Dashboard from "../../Pages/Dashboard/Dashboard/Dashboard";
+import MyAppoinment from "../../Pages/Dashboard/MyAppoinment/MyAppoinment";
+import Home from "../../Pages/Home/Home/Home";
+import Login from "../../Pages/Login/Login";
+import SignUp from "../../Pages/SignUp/SignUp";
+import PrivateRoute from "../PrivateRoute/PrivateRoute";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>, 
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
+            },
+            {
+                path: '/signup',
+                element: <SignUp></SignUp>
+            },
+            {
+                path: '/appointment',
+                element: <Appointment></Appointment>
+            }
+        ]
+    },
+    {
+        path: '/dashboard',
+        element: <PrivateRoute><DashboardLayout/></PrivateRoute>,
+        children: [
+            {
+                path: '/dashboard',
+                element: <MyAppoinment/>
+            }
+        ]
+    }
+])
+
+export default router;
